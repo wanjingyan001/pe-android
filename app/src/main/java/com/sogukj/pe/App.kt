260a3c63b837a9e7bbc971aef8375ca1acf5d2ad
qinfei <qinff@doudouxing.com>
2017-09-12 19:05:44 +0800

    push

--- app/src/main/java/com/sogukj/pe/App.kt
+++ app/src/main/java/com/sogukj/pe/App.kt
@@ -1,6 +1,9 @@
 package com.sogukj.pe
 
 import android.support.multidex.MultiDexApplication
+import com.umeng.message.IUmengRegisterCallback
+import com.umeng.message.PushAgent
+
 
 /**
  * Created by qinfei on 17/7/18.
@@ -9,6 +12,19 @@ class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         INSTANCE = this
+        val mPushAgent = PushAgent.getInstance(this)
+        mPushAgent.setDebugMode(false)
+        mPushAgent.register(object : IUmengRegisterCallback {
+
+            override fun onSuccess(deviceToken: String) {
+                //注册成功会返回device token
+                println("IUmengRegisterCallback:$deviceToken")
+            }
+
+            override fun onFailure(s: String, s1: String) {
+                println("IUmengRegisterCallback:$s=>$s1")
+            }
+        })
     }
 
     companion object {
