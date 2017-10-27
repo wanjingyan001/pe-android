97bf69fea81738bf902507e21cc2b6f39c88deb4
qinfei <qinff@doudouxing.com>
2017-10-27 14:18:09 +0800

    approve

---
+++ app/src/main/java/com/sogukj/pe/ui/approve/ApproveSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.sogukj.pe.ui.approve
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ApproveSearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_approve_search)
+    }
+}
