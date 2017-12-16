f3ad5624ceb73b9c922b49fa1764860d69cdf342
sogubaby <liuxj@sogukj.com>
2017-12-16 14:44:22 +0800

    temp

--- app/src/main/java/com/sogukj/pe/ui/score/RateFragment.kt
+++ app/src/main/java/com/sogukj/pe/ui/score/RateFragment.kt
@@ -23,8 +23,10 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.RoundRectShape
+import android.support.v7.widget.RecyclerView
 import android.widget.TextView
 import com.sogukj.pe.util.Utils
+import com.sogukj.pe.view.MyListView
 import io.reactivex.Observable
 import io.reactivex.functions.Consumer
 import io.reactivex.functions.Function
@@ -54,51 +56,25 @@ class RateFragment : BaseFragment() {
         get() = R.layout.fragment_rate
 
     var type = 0
+    var ItemType = 0//0--无title，1-有title
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         type = arguments.getInt(Extras.TYPE)
+        if (type == TYPE_JOB) {
+            ItemType = 0
+        } else if (type == TYPE_RATE) {
+            ItemType = 1
+        }
 
-        adapter = RecyclerAdapter<WeeklySendBean>(context, { _adapter, parent, type0 ->
-            val convertView = _adapter.getView(R.layout.item_rate, parent) as LinearLayout
-            object : RecyclerHolder<WeeklySendBean>(convertView) {
-
-                var bar = convertView.findViewById(R.id.progressBar) as ProgressBar
-                var judge = convertView.findViewById(R.id.text) as TextView
-                var title = convertView.findViewById(R.id.title) as TextView
-                var desc = convertView.findViewById(R.id.desc) as TextView
-                var lll = convertView.findViewById(R.id.lll) as LinearLayout
-
-                override fun setData(view: View, data: WeeklySendBean, position: Int) {
-                    if (type == TYPE_JOB) {
-                        lll.visibility = View.GONE
-                    } else if (type == TYPE_RATE) {
-
-                    }
-
-                    var obser = TextViewClickObservable(context, judge, bar)
-                    observable_List.add(obser)
-
-                    if (position == adapter.dataList.size - 1) {
-                        Observable.combineLatest(observable_List, object : Function<Array<Any>, Boolean> {
-                            override fun apply(str: Array<Any>): Boolean {
-                                return true//isEmailValid(str[0].toString()) && isPasswordValid(str[1].toString())
-                            }
-                        }).subscribe(object : Consumer<Boolean> {
-                            override fun accept(t: Boolean) {
-                                if (t == true) {
-                                    tv_socre.text = "98.00"
-                                    btn_commit.setBackgroundColor(Color.parseColor("#FFE95C4A"))
-                                    btn_commit.setOnClickListener {
-
-                                    }
-                                }
-                            }
-                        })
-                    }
-                }
+        adapter = RecyclerAdapter<WeeklySendBean>(context, { _adapter, parent, t ->
+            when (ItemType) {
+                0 -> ProjectHolderNoTitle(_adapter.getView(R.layout.item_rate, parent))
+                1 -> ProjectHolderTitle(_adapter.getView(R.layout.item_rate_title, parent))
+                else -> throw IllegalArgumentException()
             }
+
         })
         adapter.onItemClick = { v, p ->
         }
@@ -112,8 +88,78 @@ class RateFragment : BaseFragment() {
         adapter.dataList.add(WeeklySendBean())
         adapter.dataList.add(WeeklySendBean())
         adapter.dataList.add(WeeklySendBean())
+        adapter.dataList.add(WeeklySendBean())
+        adapter.dataList.add(WeeklySendBean())
         adapter.notifyDataSetChanged()
     }
 
     val observable_List = ArrayList<Observable<CharSequence>>()
+
+    inner class ProjectHolderNoTitle(view: View)
+        : RecyclerHolder<WeeklySendBean>(view) {
+
+        var bar = convertView.findViewById(R.id.progressBar) as ProgressBar
+        var judge = convertView.findViewById(R.id.text) as TextView
+        var title = convertView.findViewById(R.id.title) as TextView
+        var desc = convertView.findViewById(R.id.desc) as TextView
+        var lll = convertView.findViewById(R.id.lll) as LinearLayout
+
+        override fun setData(view: View, data: WeeklySendBean, position: Int) {
+            if (type == TYPE_JOB) {
+                lll.visibility = View.GONE
+            } else if (type == TYPE_RATE) {
+
+            }
+
+            var obser = TextViewClickObservable(context, judge, bar)
+            observable_List.add(obser)
+
+            if (position == adapter.dataList.size - 1) {
+                Observable.combineLatest(observable_List, object : Function<Array<Any>, Boolean> {
+                    override fun apply(str: Array<Any>): Boolean {
+                        return true//isEmailValid(str[0].toString()) && isPasswordValid(str[1].toString())
+                    }
+                }).subscribe(object : Consumer<Boolean> {
+                    override fun accept(t: Boolean) {
+                        if (t == true) {
+                            tv_socre.text = "98.00"
+                            btn_commit.setBackgroundColor(Color.parseColor("#FFE95C4A"))
+                            btn_commit.setOnClickListener {
+
+                            }
+                        }
+                    }
+                })
+            }
+        }
+
+    }
+
+    inner class ProjectHolderTitle(view: View)
+        : RecyclerHolder<WeeklySendBean>(view) {
+
+        var title = convertView.findViewById(R.id.head_title) as TextView
+        var data_list = convertView.findViewById(R.id.listview) as RecyclerView
+
+        override fun setData(view: View, data: WeeklySendBean, position: Int) {
+            var inner_adapter = RecyclerAdapter<WeeklySendBean>(context, { _adapter, parent, t ->
+                ProjectHolderNoTitle(_adapter.getView(R.layout.item_rate, parent))
+            })
+            inner_adapter.onItemClick = { v, p ->
+            }
+            val layoutManager = LinearLayoutManager(context)
+            layoutManager.orientation = LinearLayoutManager.VERTICAL
+            data_list.layoutManager = layoutManager
+            data_list.addItemDecoration(SpaceItemDecoration(25))
+            data_list.adapter = inner_adapter
+
+
+            inner_adapter.dataList.add(WeeklySendBean())
+            inner_adapter.dataList.add(WeeklySendBean())
+            inner_adapter.dataList.add(WeeklySendBean())
+            inner_adapter.dataList.add(WeeklySendBean())
+            inner_adapter.dataList.add(WeeklySendBean())
+            inner_adapter.notifyDataSetChanged()
+        }
+    }
 }
