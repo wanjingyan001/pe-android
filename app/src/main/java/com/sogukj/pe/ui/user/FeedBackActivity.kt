38b376c409b096639e1c520448d584571ca770f9
sogubaby <liuxj@sogukj.com>
2018-02-05 17:33:13 +0800

    意见反馈

--- app/src/main/java/com/sogukj/pe/ui/user/FeedBackActivity.kt
+++ app/src/main/java/com/sogukj/pe/ui/user/FeedBackActivity.kt
@@ -8,6 +8,7 @@ import com.sogukj.pe.R
 import com.sogukj.pe.util.Trace
 import com.sogukj.pe.util.Utils
 import com.sogukj.service.SoguApi
+import com.sogukj.util.Store
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_feed_back.*
@@ -36,18 +37,35 @@ class FeedBackActivity : BaseActivity() {
 
 
     fun submit() {
-        if (feedEdt.text.toString().isEmpty()) {
-            showToast("请填写意见建议")
-            return
-        }
-        if (contactInfoEdt.text.toString().isEmpty()) {
-            showToast("请填写联系方式")
-            return
-        }
+//        if (feedEdt.text.toString().isEmpty()) {
+//            showToast("请填写意见建议")
+//            return
+//        }
+//        if (contactInfoEdt.text.toString().isEmpty()) {
+//            showToast("请填写联系方式")
+//            return
+//        }
+//        SoguApi.getService(application)
+//                .addFeedback(feedEdt.text.toString(),
+//                        contactEdt.text.toString(),
+//                        contactInfoEdt.text.toString())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribeOn(Schedulers.io())
+//                .subscribe({ payload ->
+//                    if (payload.isOk) {
+//                        showToast("提交成功")
+//                        finish()
+//                    } else {
+//                        showToast(payload.message)
+//                    }
+//                }, { e ->
+//                    Trace.e(e)
+//                })
+        var user = Store.store.getUser(context)
         SoguApi.getService(application)
                 .addFeedback(feedEdt.text.toString(),
-                        contactEdt.text.toString(),
-                        contactInfoEdt.text.toString())
+                        user?.name,
+                        user?.phone!!)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
                 .subscribe({ payload ->
