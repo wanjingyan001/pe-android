068367cf67ae0b55b0ef594a190ff283e6b3238d
sogubaby <liuxj@sogukj.com>
2018-02-09 10:35:40 +0800

    关注搜索

--- app/src/main/java/com/sogukj/pe/ui/news/MainNewsFragment.kt
+++ app/src/main/java/com/sogukj/pe/ui/news/MainNewsFragment.kt
@@ -306,7 +306,11 @@ class MainNewsFragment : BaseFragment() {
                 .subscribe({ payload ->
                     if (payload.isOk) {
                         payload?.apply {
-                            tv_result_title.text = Html.fromHtml(getString(R.string.tv_title_result_news, (total as Double).toInt()))
+                            try {
+                                tv_result_title.text = Html.fromHtml(getString(R.string.tv_title_result_news, (total as Double).toInt()))
+                            } catch (e: Exception) {
+                                tv_result_title.text = Html.fromHtml(getString(R.string.tv_title_result_news, payload.payload?.size))
+                            }
                         }
                         if (page == 1)
                             adapter.dataList.clear()
