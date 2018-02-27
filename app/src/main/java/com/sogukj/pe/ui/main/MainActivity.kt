8e519494f50fb09c5bf96c5aa2c68548aac76b5b
sogubaby <liuxj@sogukj.com>
2018-02-27 13:56:35 +0800

    自动更新

--- app/src/main/java/com/sogukj/pe/ui/main/MainActivity.kt
+++ app/src/main/java/com/sogukj/pe/ui/main/MainActivity.kt
@@ -2,13 +2,17 @@ package com.sogukj.pe.ui.main
 
 import android.Manifest
 import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.graphics.Rect
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.FragmentManager
+import android.support.v4.content.FileProvider
 import com.framework.base.BaseActivity
 import com.sogukj.pe.App
 import com.sogukj.pe.Extras
@@ -29,10 +33,16 @@ import android.view.View
 import android.widget.*
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.Theme
+import com.sogukj.pe.util.DownloadUtil
+import com.sogukj.pe.util.Trace
 import com.sogukj.pe.util.Utils
 import com.sogukj.pe.view.ArrayPagerAdapter
+import com.sogukj.service.SoguApi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import org.jetbrains.anko.find
 import org.jetbrains.anko.imageResource
+import java.io.File
 
 
 /**
@@ -105,6 +115,8 @@ class MainActivity : BaseActivity() {
 
         manager = supportFragmentManager
         manager.beginTransaction().add(R.id.container, fragments[2]).commit()
+
+        updateVersion()
     }
 
     lateinit var manager: FragmentManager
@@ -188,9 +200,144 @@ class MainActivity : BaseActivity() {
             }
         }
     }
+    
+    private fun updateVersion() {
+        val mDialog = MaterialDialog.Builder(this)
+                .customView(R.layout.dialog_updated, false)
+                .theme(Theme.LIGHT)
+                .cancelable(false)
+                .build()
+        mDialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        val icon = mDialog.find<ImageView>(R.id.updated_icon)
+        val bar = mDialog.find<ProgressBar>(R.id.update_progress)
+        val title = mDialog.find<TextView>(R.id.update_title)
+        val update_info = mDialog.find<TextView>(R.id.update_info)
+        val scrollView = mDialog.find<ScrollView>(R.id.update_message_layout)
+        val message = mDialog.find<TextView>(R.id.update_message)
+        val update = mDialog.find<Button>(R.id.update)
+        val cancelBtn = mDialog.find<Button>(R.id.cancel)
+        val prompt = mDialog.find<TextView>(R.id.update_prompt)
+        mDialog.show()
+        title.text = "正在检测更新"
+        update_info.visibility = View.INVISIBLE
+        scrollView.visibility = View.INVISIBLE
+        update.visibility = View.INVISIBLE
+        cancelBtn.visibility = View.INVISIBLE
+        prompt.visibility = View.INVISIBLE
+
+        SoguApi.getService(application)
+                .getVersion()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({ payload ->
+                    if (payload.isOk) {
+                        var newVersion = payload.payload?.version
+                        var info = payload.payload?.info
+                        var url = payload.payload?.app_url
+                        var force = payload.payload?.force//1=>更新，0=>不更新，2---强制更新
+
+                        if (!url.isNullOrEmpty()) {
+                            if (force == 0) {
+                                title.text = "不用更新"
+                                mDialog.dismiss()
+                                //enterNext()
+                            } else if (force == 1) {
+                                update_info.visibility = View.VISIBLE
+                                scrollView.visibility = View.VISIBLE
+                                update.visibility = View.VISIBLE
+                                cancelBtn.visibility = View.VISIBLE//TODO
+                                title.text = "当前版本过低(" + Utils.getVersionName(context) + ")"
+                                update_info.text = "请立即升级到最新版(" + newVersion + ")"
+                                icon.imageResource = R.drawable.update_icon1
+                                scrollView.visibility = View.VISIBLE
+                                message.text = info.toString()
+                                update.setOnClickListener {
+                                    update.visibility = View.GONE
+                                    cancelBtn.visibility = View.GONE
+                                    update(url!!, bar, title, mDialog)
+                                }
+                                cancelBtn.setOnClickListener {
+                                    mDialog.dismiss()
+                                }
+                            } else if (force == 2) {
+                                update_info.visibility = View.VISIBLE
+                                scrollView.visibility = View.VISIBLE
+                                update.visibility = View.VISIBLE
+                                cancelBtn.visibility = View.GONE//TODO
+                                title.text = "当前版本过低(" + Utils.getVersionName(context) + ")"
+                                update_info.text = "请立即升级到最新版(" + newVersion + ")"
+                                icon.imageResource = R.drawable.update_icon1
+                                scrollView.visibility = View.VISIBLE
+                                message.text = info.toString()
+                                update.setOnClickListener {
+                                    update.visibility = View.GONE
+                                    update(url!!, bar, title, mDialog)
+                                }
+                            }
+                        } else {
+                            //enterNext()
+                            title.text = "更新失败"
+                            mDialog.dismiss()
+                        }
+                    } else {
+                        showToast(payload.message)
+                        //enterNext()
+                        title.text = "更新失败"
+                        mDialog.dismiss()
+                    }
+                }, { e ->
+                    Trace.e(e)
+                    title.text = "更新失败"
+                    //showToast("更新失败，进入首页")
+                    //enterNext()
+                    mDialog.dismiss()
+                })
+    }
 
     override fun onResume() {
         super.onResume()
         App.INSTANCE.resetPush(true)
+    }
+
+    fun update(url: String, bar: ProgressBar, title: TextView, dialog: MaterialDialog) {
+        bar.visibility = View.VISIBLE
+        title.text = "开始下载"
+        val fileName = url.substring(url.lastIndexOf("/") + 1)
+        DownloadUtil.getInstance().download(url, externalCacheDir.toString(), fileName, object : DownloadUtil.OnDownloadListener {
+            override fun onDownloadSuccess(path: String?) {
+                title.text = "下载完成"
+                dialog.dismiss()
+                var intent = Intent()
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                intent.action = Intent.ACTION_VIEW
+                //val uri = Uri.fromFile(File(path))
+                val uri = transform(path!!, intent)
+                intent.setDataAndType(uri, "application/vnd.android.package-archive")
+                startActivity(intent)
+            }
+
+            override fun onDownloading(progress: Int) {
+                title.text = "已下载" + progress + "%"
+                bar.progress = progress
+            }
+
+            override fun onDownloadFailed() {
+                //showToast("下载失败")
+                title.text = "下载失败"
+            }
+        })
+    }
+
+    private fun transform(param: String, intent: Intent): Uri {
+        val uri: Uri
+        // 判断版本大于等于7.0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            uri = FileProvider.getUriForFile(context, "com.sogukj.pe.fileProvider", File(param))
+            //uri = MyFileProvider.getUriForFile(new File(param));
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+        } else {
+            uri = Uri.fromFile(File(param))
+        }
+        return uri
     }
 }
