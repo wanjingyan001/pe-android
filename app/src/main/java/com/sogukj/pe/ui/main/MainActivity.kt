38c13942bf7a99a3be2b6aec98d9b58109615327
sogubaby <liuxj@sogukj.com>
2018-02-28 17:10:37 +0800

    弹窗-审批、版本更新

--- app/src/main/java/com/sogukj/pe/ui/main/MainActivity.kt
+++ app/src/main/java/com/sogukj/pe/ui/main/MainActivity.kt
@@ -2,11 +2,13 @@ package com.sogukj.pe.ui.main
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.graphics.Rect
 import android.graphics.drawable.ColorDrawable
+import android.net.ConnectivityManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -29,6 +31,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.support.v4.view.ViewCompat.getMinimumHeight
 import android.support.v4.view.ViewCompat.getMinimumWidth
 import android.support.v4.view.ViewPager
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.widget.*
 import com.afollestad.materialdialogs.MaterialDialog
@@ -40,6 +45,7 @@ import com.sogukj.pe.view.ArrayPagerAdapter
 import com.sogukj.service.SoguApi
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.item_comment_list.*
 import org.jetbrains.anko.find
 import org.jetbrains.anko.imageResource
 import java.io.File
@@ -200,7 +206,7 @@ class MainActivity : BaseActivity() {
             }
         }
     }
-    
+
     private fun updateVersion() {
         val mDialog = MaterialDialog.Builder(this)
                 .customView(R.layout.dialog_updated, false)
@@ -215,15 +221,20 @@ class MainActivity : BaseActivity() {
         val scrollView = mDialog.find<ScrollView>(R.id.update_message_layout)
         val message = mDialog.find<TextView>(R.id.update_message)
         val update = mDialog.find<Button>(R.id.update)
-        val cancelBtn = mDialog.find<Button>(R.id.cancel)
         val prompt = mDialog.find<TextView>(R.id.update_prompt)
         mDialog.show()
         title.text = "正在检测更新"
-        update_info.visibility = View.INVISIBLE
-        scrollView.visibility = View.INVISIBLE
-        update.visibility = View.INVISIBLE
-        cancelBtn.visibility = View.INVISIBLE
-        prompt.visibility = View.INVISIBLE
+        update_info.visibility = View.GONE
+        scrollView.visibility = View.GONE
+        update.visibility = View.GONE
+        prompt.visibility = View.VISIBLE
+        if (isWifi()) {
+            prompt.text = "当前处于Wi-Fi网络，请放心下载"
+        } else {
+            var hint = SpannableString("当前处于4G网络，下载将消耗流量")
+            hint.setSpan(ForegroundColorSpan(Color.parseColor("#FF6174")), 9, hint.length, Spanned.SPAN_INCLUSIVE_EXCLUSIVE)
+            prompt.text = hint
+        }
 
         SoguApi.getService(application)
                 .getVersion()
@@ -239,39 +250,29 @@ class MainActivity : BaseActivity() {
                         if (!url.isNullOrEmpty()) {
                             if (force == 0) {
                                 title.text = "不用更新"
+                                Thread.sleep(500)
                                 mDialog.dismiss()
                                 //enterNext()
-                            } else if (force == 1) {
+                            } else {//1=>更新，2---强制更新
+                                if (force == 1) {
+                                    mDialog.setCanceledOnTouchOutside(true)
+                                } else {
+                                    mDialog.setCanceledOnTouchOutside(false)
+                                }
                                 update_info.visibility = View.VISIBLE
-                                scrollView.visibility = View.VISIBLE
                                 update.visibility = View.VISIBLE
-                                cancelBtn.visibility = View.VISIBLE//TODO
                                 title.text = "当前版本过低(" + Utils.getVersionName(context) + ")"
                                 update_info.text = "请立即升级到最新版(" + newVersion + ")"
-                                icon.imageResource = R.drawable.update_icon1
-                                scrollView.visibility = View.VISIBLE
+                                scrollView.visibility = View.GONE
                                 message.text = info.toString()
                                 update.setOnClickListener {
+                                    icon.imageResource = R.drawable.update_icon1
                                     update.visibility = View.GONE
-                                    cancelBtn.visibility = View.GONE
-                                    update(url!!, bar, title, mDialog)
-                                }
-                                cancelBtn.setOnClickListener {
-                                    mDialog.dismiss()
-                                }
-                            } else if (force == 2) {
-                                update_info.visibility = View.VISIBLE
-                                scrollView.visibility = View.VISIBLE
-                                update.visibility = View.VISIBLE
-                                cancelBtn.visibility = View.GONE//TODO
-                                title.text = "当前版本过低(" + Utils.getVersionName(context) + ")"
-                                update_info.text = "请立即升级到最新版(" + newVersion + ")"
-                                icon.imageResource = R.drawable.update_icon1
-                                scrollView.visibility = View.VISIBLE
-                                message.text = info.toString()
-                                update.setOnClickListener {
-                                    update.visibility = View.GONE
-                                    update(url!!, bar, title, mDialog)
+                                    title.text = "新版功能介绍"
+                                    update_info.visibility = View.GONE
+                                    scrollView.visibility = View.VISIBLE
+                                    prompt.visibility = View.GONE
+                                    update(url!!, bar, update, mDialog, prompt, force!!)
                                 }
                             }
                         } else {
@@ -294,36 +295,64 @@ class MainActivity : BaseActivity() {
                 })
     }
 
+    private fun isWifi(): Boolean {
+        var connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        var activeNetInfo = connectivityManager.getActiveNetworkInfo()
+        if (activeNetInfo != null && activeNetInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+            return true
+        }
+        return false
+    }
+
     override fun onResume() {
         super.onResume()
         App.INSTANCE.resetPush(true)
     }
 
-    fun update(url: String, bar: ProgressBar, title: TextView, dialog: MaterialDialog) {
+    fun update(url: String, bar: ProgressBar, update: Button, dialog: MaterialDialog, prompt: TextView, force: Int) {
         bar.visibility = View.VISIBLE
-        title.text = "开始下载"
+        //title.text = "开始下载"
         val fileName = url.substring(url.lastIndexOf("/") + 1)
         DownloadUtil.getInstance().download(url, externalCacheDir.toString(), fileName, object : DownloadUtil.OnDownloadListener {
             override fun onDownloadSuccess(path: String?) {
-                title.text = "下载完成"
-                dialog.dismiss()
-                var intent = Intent()
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                intent.action = Intent.ACTION_VIEW
-                //val uri = Uri.fromFile(File(path))
-                val uri = transform(path!!, intent)
-                intent.setDataAndType(uri, "application/vnd.android.package-archive")
-                startActivity(intent)
+                //title.text = "下载完成"
+                //dialog.dismiss()
+                if (force == 1) {
+                    dialog.setCanceledOnTouchOutside(true)
+                } else {
+                    dialog.setCanceledOnTouchOutside(false)
+                }
+                bar.visibility = View.GONE
+                update.visibility = View.VISIBLE
+                prompt.visibility = View.VISIBLE
+                update.text = "立刻安装"
+                if (isWifi()) {
+                    prompt.text = "已在Wi-Fi网络下完成下载"
+                } else {
+                    prompt.text = "已在4G网络下完成下载"
+                }
+                update.setOnClickListener {
+                    dialog.dismiss()
+                    var intent = Intent()
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    intent.action = Intent.ACTION_VIEW
+                    //val uri = Uri.fromFile(File(path))
+                    val uri = transform(path!!, intent)
+                    intent.setDataAndType(uri, "application/vnd.android.package-archive")
+                    startActivity(intent)
+                }
             }
 
             override fun onDownloading(progress: Int) {
-                title.text = "已下载" + progress + "%"
+                //title.text = "已下载" + progress + "%"
                 bar.progress = progress
+                dialog.setCanceledOnTouchOutside(false)
+                dialog.setCancelable(false)
             }
 
             override fun onDownloadFailed() {
                 //showToast("下载失败")
-                title.text = "下载失败"
+                //title.text = "下载失败"
             }
         })
     }
